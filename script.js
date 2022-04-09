f08509e883622fb59cada911c70022e14c469f0b
Aer-osa <eric.n.long@gmail.com>
2022-04-09 09:29:54 -0700

    Create external js file

---
+++ script.js
@@ -0,0 +1,44 @@
+const choices = ['Rock', 'Paper', 'Scissors']
+
+const pcChoice = choices[Math.floor(Math.random() * choices.length)];
+   console.log("PC: " + pcChoice)
+
+let playerChoice = prompt("Please enter Rock, Paper or Scissors").toLowerCase();
+   console.log("Player " + playerChoice)
+
+function game (){
+console.log(playGame())
+} 
+
+function playGame(){
+   if ((playerChoice ==='paper') && (pcChoice === 'scissors')){
+       return 'You Lose!';
+
+    } else if ((playerChoice === 'paper') && (pcChoice === 'rock')) {
+       return "You Win!";
+
+     } else if ((playerChoice === 'scissors') && (pcChoice === 'rock')) {
+       return "You Lose!";
+   
+      } else if ((playerChoice === 'scissors') && (pcChoice === 'paper')) {
+       return "You Win!";
+
+       } else if ((playerChoice === 'rock') && (pcChoice === 'scissors')) {
+       return "You Win!";  
+
+        } else if ((playerChoice === 'rock') && (pcChoice === 'paper')) {
+        return "You Lose!";
+
+         } else if (playerChoice === "Rock" && pcChoice === "Rock") {
+           return "It's a tie! You both picked Rock.";
+  
+            } else if (playerChoice === "Scissors" && pcChoice === "Scissors") {
+           return "It's a tie! You both picked Scissors.";
+
+            } else if (playerChoice === "Paper" && pcChoice === "Paper") {
+           return "It's a tie! You both picked Paper.";
+              
+            } else 
+            return "Tie!";
+
+}
