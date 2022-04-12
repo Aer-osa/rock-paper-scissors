3b531f13cbea761dd4f08fbf17b38324768b5240
Aer-osa <eric.n.long@gmail.com>
2022-04-12 15:03:20 -0700

    Fix playRound

--- altscript.js
+++ altscript.js
@@ -9,44 +9,44 @@ let playerChoice = prompt("Please enter Rock, Paper or Scissors").toLowerCase();
 
 function playGame(playerChoice, pcChoice){
    if ((playerChoice ==='paper') && (pcChoice === 'scissors')){
-       return prompt("You Lose!");
+       console.log("You Lose!");
 
     } else if ((playerChoice === 'paper') && (pcChoice === 'rock')) {
-       prompt("You Win!");
+       console.log("You Win!");
 
      } else if ((playerChoice === 'scissors') && (pcChoice === 'rock')) {
-       prompt("You Lose!");
+       console.log("You Lose!");
    
       } else if ((playerChoice === 'scissors') && (pcChoice === 'paper')) {
-       prompt("You Win!");
+       console.log("You Win!");
 
        } else if ((playerChoice === 'rock') && (pcChoice === 'scissors')) {
-       prompt("You Win!");  
+       console.log("You Win!");  
 
         } else if ((playerChoice === 'rock') && (pcChoice === 'paper')) {
-        prompt("You Lose!");
+        console.log("You Lose!");
 
          } else if (playerChoice === "Rock" && pcChoice === "Rock") {
-           prompt("It's a tie! You both picked Rock.");
+           console.log("It's a tie! You both picked Rock.");
   
             } else if (playerChoice === "Scissors" && pcChoice === "Scissors") {
-            prompt("It's a tie! You both picked Scissors.");
+            console.log("It's a tie! You both picked Scissors.");
 
             } else if (playerChoice === "Paper" && pcChoice === "Paper") {
-            prompt("It's a tie! You both picked Paper.");
+            console.log("It's a tie! You both picked Paper.");
               
             } else 
-            prompt("Tie!");
+            console.log("Tie!");
 
 } 
 function playRound(playerChoice, pcChoice){
     for (let i = 0; i < 5; i++){
         console.log("Round: " + i);
         let playerChoice = prompt("Please enter Rock, Paper or Scissors").toLowerCase();
-   console.log("Player " + playerChoice)
-   const pcChoice =  choices[Math.floor(Math.random() * choices.length)];
-   console.log("PC: " + pcChoice)
-   function playGame(playerChoice, pcChoice)
+        console.log("Player " + playerChoice)
+        const pcChoice = choices[Math.floor(Math.random() * choices.length)];
+        console.log("PC: " + pcChoice)
+   
+
     }
 }
-playRound(playerChoice, pcChoice)
